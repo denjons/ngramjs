05eadce081b1cc84d1d7665590c4eedbe08cea61
Dennis Jönsson <dennis.j@live.se>
2020-09-14 20:21:37 +0200

    Updates

---
+++ precache-manifest.10745ccaad3c669a711d5c27bb4010d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ad8da6166b11a0ebd82",
+    "url": "/ngramjs/static/js/runtime~main.9ad8da61.js"
+  },
+  {
+    "revision": "6cb2a53cb6bee4a22de2",
+    "url": "/ngramjs/static/js/main.6cb2a53c.chunk.js"
+  },
+  {
+    "revision": "0063609cb55560ff0781",
+    "url": "/ngramjs/static/js/2.0063609c.chunk.js"
+  },
+  {
+    "revision": "6cb2a53cb6bee4a22de2",
+    "url": "/ngramjs/static/css/main.02ce825d.chunk.css"
+  },
+  {
+    "revision": "0063609cb55560ff0781",
+    "url": "/ngramjs/static/css/2.d23d8313.chunk.css"
+  },
+  {
+    "revision": "95b3d24c0a267dc07704af36276b23b6",
+    "url": "/ngramjs/index.html"
+  }
+];
